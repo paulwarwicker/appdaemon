65e2458ba9be2d9e243073fd808f9a1a077fb2e3
Paul Warwicker <paul.warwicker@gmail.com>
2024-12-22 22:47:50 +0000

    refactor weather

--- apps/weather.py
+++ apps/weather.py
@@ -156,41 +156,42 @@ class Weather(Hass):
         data[2] = json_data['timelines']['hourly'][index + 2] # hour after
         data[3] = json_data['timelines']['hourly'][index + 3] # hour after
 
-        if self.lib.get_verbose_debug():
-            print(json.dumps(data[0], indent=2))
-            print(json.dumps(data[1], indent=2))
-            print(json.dumps(data[2], indent=2))
-            print(json.dumps(data[3], indent=2))
+        # if self.lib.get_verbose_debug():
+        #     print(json.dumps(data[0], indent=2))
+        #     print(json.dumps(data[1], indent=2))
+        #     print(json.dumps(data[2], indent=2))
+        #     print(json.dumps(data[3], indent=2))
 
         code  = data[0]['values']['weatherCode']
-        # code  = data[1]['values']['weatherCode']
-        self.log(f'\tcode={code}', level='WARNING')
-        # cum_prob_1h = math.ceil((data[0]['values']['precipitationProbability'] + data[1]['values']['precipitationProbability']) / 2)
+
+        # cum_prob_1h = math.ceil((data[0]['values']['precipitationProbability'] +
+        #                          data[1]['values']['precipitationProbability']) / 2)
+        # cum_prob_3h = math.ceil((data[0]['values']['precipitationProbability'] +
+        #                          data[1]['values']['precipitationProbability'] +
+        #                          data[2]['values']['precipitationProbability'] +
+        #                          data[3]['values']['precipitationProbability'] ) / 4)
         cum_prob_1h = math.ceil(data[1]['values']['precipitationProbability'])
-        # cum_prob_3h = math.ceil((data[0]['values']['precipitationProbability'] + data[1]['values']['precipitationProbability'] +
-        #                          data[2]['values']['precipitationProbability'] + data[3]['values']['precipitationProbability'] ) / 4)
         cum_prob_3h = math.ceil((data[1]['values']['precipitationProbability'] +
-                                 data[2]['values']['precipitationProbability'] + data[3]['values']['precipitationProbability'] ) / 3)
+                                 data[2]['values']['precipitationProbability'] +
+                                 data[3]['values']['precipitationProbability']) / 3)
 
         if cum_prob_1h != 0 and cum_prob_3h != 0:
-            self.log(f'\tprobability of rain1h={cum_prob_1h}, rain3h={cum_prob_3h}', level='DEBUG')
+            self.log(f'\tcode={code} probability of rain1h={cum_prob_1h}, rain3h={cum_prob_3h}', level='DEBUG')
 
         await self.set_state(
             'sensor.weather_tomorrowio_forecast_rain_probability_1h',
             state=cum_prob_1h,
-            device_class='probability',
-        )
-
-        if cum_prob_1h >= 5:
-            pct = math.floor(cum_prob_1h)
-            if not code in (4000, 4001, 4200, 4201): # if not raining - announce
-                self.call_service('announcer/announce', entity_id='media_player.study', message=f"There is a {pct} percent probability of rain in the next hour")
+            device_class='probability')
 
         await self.set_state(
             'sensor.weather_tomorrowio_forecast_rain_probability_3h',
             state=math.floor(cum_prob_3h),
-            device_class='probability',
-        )
+            device_class='probability')
+
+        if cum_prob_1h >= 35:
+            pct = math.floor(cum_prob_1h)
+            if not code in (4000, 4001, 4200, 4201): # if not raining - announce
+                self.call_service('announcer/announce', entity_id='media_player.study', message=f"There is a {pct} percent probability of rain in the next hour")
 
 # -----------------------------------------------------------------------------------
 
