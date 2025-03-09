674fcfa01527c3373521c17c1d0f6566336fc0d3
Paul Warwicker <paul.warwicker@gmail.com>
2025-03-09 20:13:14 +0000

    adjust normal alarm streams

--- apps/const.py
+++ apps/const.py
@@ -109,6 +109,13 @@ class AutomationConstants:
     # NORMAL_ALARM = ['aac://http://prem2.radiotunes.com:80/80saltnnewwave?5fba91be81f6da5b573f89c1',
     #                 'aac://http://prem4.radiotunes.com:80/80saltnnewwave?5fba91be81f6da5b573f89c1',
     #                 'aac://http://prem1.radiotunes.com:80/80saltnnewwave?5fba91be81f6da5b573f89c1']
+    NORMAL_ALARM = ['aac://http://prem2.di.fm:80/progressive?5fba91be81f6da5b573f89c1',
+                    'aac://http://prem1.di.fm:80/progressive?5fba91be81f6da5b573f89c1',
+                    'aac://http://prem4.di.fm:80/progressive?5fba91be81f6da5b573f89c1',
+                    'aac://http://prem2.radiotunes.com:80/80saltnnewwave?5fba91be81f6da5b573f89c1',
+                    'aac://http://prem4.radiotunes.com:80/80saltnnewwave?5fba91be81f6da5b573f89c1',
+                    'aac://http://prem1.radiotunes.com:80/80saltnnewwave?5fba91be81f6da5b573f89c1']
+
     CHRISTMAS_STREAM = 'aac://http://prem2.radiotunes.com:80/popchristmas?5fba91be81f6da5b573f89c1'
     BOSSANOVA_STREAM = 'aac://http://prem2.radiotunes.com:80/smoothbossanova?5fba91be81f6da5b573f89c1'
 
